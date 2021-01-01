51bbf10d759ef788933e59f64200c5b061a4c61b
TJ Wetmore <Thomas.J.Wetmore@gmail.com>
2021-01-01 11:08:06 -0500

    don't worry about these

--- webpack.config.js
+++ webpack.config.js
@@ -4,36 +4,57 @@ const HtmlWepackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  entry: {
-    main: path.resolve(__dirname, './client/index.js'),
-  },
-  output: {
-    path: path.resolve(__dirname, './dist'),
-    filename: '[name].bundle.js',
+  mode: process.env.NODE_ENV,
+  entry: './client/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/i,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+            plugins: ['@babel/plugin-syntax-dynamic-import', '@babel/plugin-proposal-class-properties'],
+          },
+        },
+      },
+      {
+        // CSS, PostCSS and Sass
+        test: /\.(scss|css)$/,
+        exclude: /(node_modules)/,
+        // removed style-loader, not required in production.
+        use: [
+          'css-loader',
+          'postcss-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.(gif|png|jpg|jpeg)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192,
+            },
+          },
+        ],
+        type: 'javascript/auto',
+      },
+      {
+        // Fonts and SVGs
+        test: /\.(woff(2)?|eot|ttf|otf|svg)$/,
+        type: 'javascript/auto',
+      },
+    ],
   },
   resolve: {
     extensions: ['.js', '.jsx'],
   },
-  mode: process.env.NODE_ENV,
-  devServer: {
-    publicPath: '/',
-    historyApiFallback: true,
-    port: 8080,
-    hot: true,
-    open: true,
-    inline: true,
-    proxy: {
-      '/': {
-        target: 'http://localhost:3000/',
-        secure: false,
-        bypass: (req, res, proxyOptions) => {
-          if (req.headers.accept.indexOf('html') !== -1) {
-            // console.log('Skipping proxy for browser request.');
-            return '/index.html';
-          }
-        },
-      },
-    },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].bundle.js',
   },
   plugins: [
     new HtmlWepackPlugin({
@@ -45,34 +66,18 @@ module.exports = {
       filename: '[name].css',
     }),
   ],
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/i,
-        exclude: /node_modules/,
-        use: ['babel-loader'],
+  devServer: {
+    publicPath: '/',
+    historyApiFallback: true,
+    port: 8080,
+    hot: true,
+    open: true,
+    inline: true,
+    proxy: {
+      '/': {
+        target: 'http://localhost:3000/',
+        secure: false,
       },
-      {
-        // CSS, PostCSS and Sass
-        test: /\.(scss|css)$/,
-        exclude: /node_modules/,
-        // removed style-loader, not required in production.
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader',
-          'postcss-loader',
-          'sass-loader',
-        ],
-      },
-      {
-        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
-        type: 'asset/resource',
-      },
-      {
-        // Fonts and SVGs
-        test: /\.(woff(2)?|eot|ttf|otf|svg)$/,
-        type: 'asset/inline',
-      },
-    ],
+    },
   },
 };
